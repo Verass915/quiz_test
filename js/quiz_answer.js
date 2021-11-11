1ada5c72b585378b97c99e748a22962987304f78
Vera <75651598+Verass915@users.noreply.github.com>
2021-11-12 01:44:56 +0400

    Add files via upload

---
+++ js/quiz_answer.js
@@ -0,0 +1,17 @@
+let answer = {
+    "rip" : {
+        "description" : "rip",
+    },
+    "pochti rip" : {
+        "description" : "pochti rip",
+    },
+    "norm, no idi k vrachy" : {
+        "description" : "norm, no idi k vrachy",
+    },
+    "ne pohozhe na klinich depress" : {
+        "description" : "ne pohozhe na klinich depress, no esli ne rabotat nad psihicheskim zdorov, to sost mozhet stat huzhe",
+    },
+    "mozhet eto prosto osen" : {
+        "description" : "vradli u tebia depressiya, mozhet eto prosto osen",
+    },
+};
---
+++ js/quiz_data.js
@@ -0,0 +1,103 @@
+const quiz = [
+    {
+        "q" : " В последнее время я почти не испытываю эмоций.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : "  Сейчас мое настроение не хуже обычного.",
+        "a" : {
+            "mozhet eto prosto osen": "Совершенно верно",
+            "ne pohozhe na klinich depress": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "pochti rip": "Скорее неверно",
+            "rip": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : "У меня часто бывает подавленное настроение.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : " Я стал(-а) более обидчивым человеком, чем обычно.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : "Я часто испытываю чувство потери чего-то важного для меня.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : " Я ощущаю тяжесть на душе.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : " За свое будущее я абсолютно спокоен(-на).",
+        "a" : {
+            "mozhet eto prosto osen": "Совершенно верно",
+            "ne pohozhe na klinich depress": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "pochti rip": "Скорее неверно",
+            "rip": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : " Я стал(-а) меньше дорожить своим здоровьем.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : " Мой аппетит в последнее время не менялся.",
+        "a" : {
+            "mozhet eto prosto osen": "Совершенно верно",
+            "ne pohozhe na klinich depress": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "pochti rip": "Скорее неверно",
+            "rip": "Совершенно неверно",
+        }
+    },
+    {
+        "q" : " Я уже не надеюсь на лучшее.",
+        "a" : {
+            "rip": "Совершенно верно",
+            "pochti rip": "Скорее верно",
+            "norm, no idi k vrachy": "Затрудняюсь ответить",
+            "ne pohozhe na klinich depress": "Скорее неверно",
+            "mozhet eto prosto osen": "Совершенно неверно",
+        }
+    },
+
+]
